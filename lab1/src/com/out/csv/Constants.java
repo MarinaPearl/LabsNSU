9a3cfb915722c27161e910406460a347f5453ec7
marina <marina.demchuk.01@bk.ru>
2022-03-21 08:21:02 +0700

    )))

--- lab1/src/com/out/csv/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.out.csv;
-
-public class Constants {
-    public static final int INPUT_FILE = 0;
-    public static final int OUTPUT_FILE = 1;
-    public static final int KEY_INITIALIZATION = 0;
-    public static final int COUNTER_VALUE = 1;
-    public static final int MAX_PERCENT = 100;
-}
